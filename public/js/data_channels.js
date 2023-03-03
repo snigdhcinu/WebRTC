c59cdb6112aa6aaaa6311a5b17449ff3c8613c53
Snigdh Shourya <47975136+snigdhcinu@users.noreply.github.com>
2023-03-04 01:59:14 +0530

    add data channels (#2)

---
+++ public/js/data_channels.js
@@ -0,0 +1,45 @@
+// TODO: need to import from single source of truth, after introducing webpacks.
+const iceConfig = {
+    "iceServers" : [{
+        "urls" : 'stun:stun.l.google.com:19302',
+    }]
+};
+
+const peerConnection = new RTCPeerConnection (iceConfig);
+const dataChannel    = peerConnection.createDataChannel();
+
+// listening for any incoming message from dataChannel, as a peer.
+peerConnection.addEventListener('datachannel', (event) => {
+    const dataChannel = event.channel;
+});
+
+
+// For chat feature.
+const messageBox       = document.querySelector('#messageBox');
+const sendButton       = document.querySelector('#sendButton');
+const incomingMessages = document.querySelector('#incomingMessages');
+
+// Enable textarea and button when opened
+dataChannel.addEventListener('open', (event) => {
+    messageBox.disabled = false;
+    messageBox.focus();
+    sendButton.disabled = false;
+});
+
+// Disable input when closed
+dataChannel.addEventListener('close', (event) => {
+    messageBox.disabled = false;
+    sendButton.disabled = false;
+});
+
+// send message from the dataChannel, when clicked on send button
+sendButton.addEventListener('click', (event) => {
+    const message = messageBox.textContent;
+    dataChannel.send(message);
+})
+
+// Append new messages to the box of incoming messages
+dataChannel.addEventListener('message', event => {
+    const message = event.data;
+    incomingMessages.textContent += message + '\n';
+});
